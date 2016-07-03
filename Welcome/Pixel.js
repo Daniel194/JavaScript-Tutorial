b17de68c00ec06344b9c82bb19e642a3ac71bba8
Daniel194 <lungu.daniel94@gmail.com>
2016-07-03 14:54:38 +0300

    Take out all warning

--- Welcome/Pixel.js
+++ Welcome/Pixel.js
@@ -1,72 +1,35 @@
-var array = [[1032, 11, 82, 82], [902, 20, 78, 78], [22,
-        9,
-        77,
-        77
-    ],
-        [372, 8, 64, 64], [192,
-            30,
-            81,
-            81
-        ],
-        [550, 24, 82, 82], [516,
-            399,
-            88,
-            88
-        ],
-        [25, 202, 99, 99], [856,
-            185,
-            73,
-            73
-        ],
-        [690, 196, 89, 89], [674,
-            372,
-            93,
-            93
-        ],
-        [862, 360, 89, 89], [353,
-            223,
-            68,
-            68
-        ],
-        [528, 219, 80, 80], [1016,
-            366,
-            95,
-            95
-        ],
-        [201, 384, 82, 82], [362,
-            381,
-            70,
-            70
-        ]]
-    ;
+var array = "[[1032, 11, 82, 82], [902, 20, 78, 78], [22, 9, 77, 77 ], [372, 8, 64, 64], [192, 30, 81, 81 ], [550, 24, 82, 82], [516, 399, 88, 88 ], [25, 202, 99, 99], [856, 185, 73, 73 ], [690, 196, 89, 89], [674, 372, 93, 93 ], [862, 360, 89, 89], [353, 223, 68, 68 ], [528, 219, 80, 80], [1016, 366, 95, 95 ], [201, 384, 82, 82], [362, 381, 70, 70 ]]";
 
 $('#logo').click(function () {
-    var img = $('#logo img').attr('src');
+    var img = $('#logo').find('img');
 
-    if (img == 'image/on.png') {
+    if ($(img).attr('src') == 'image/on.png') {
         document.getElementById("mySidenav").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
-        $('#logo img').attr('src', 'image/off.png');
+        $(img).attr('src', 'image/off.png');
     } else {
         document.getElementById("mySidenav").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
-        $('#logo img').attr('src', 'image/on.png');
+        $(img).attr('src', 'image/on.png');
     }
 });
 
 
 $('.myImg').click(function () {
+    var img = $('#img01');
     $('#pictureModal').css('display', 'block');
-    $('#img01').attr('src', this.src);
-    $('#img01').attr('alt', this.alt);
+    $(img).attr('src', this.src);
+    $(img).attr('alt', this.alt);
     $('#caption').html(this.alt);
 
     var naturalHeight = this.naturalHeight;
     var naturalWidth = this.naturalWidth;
-    var height = $('#img01').height();
-    var width = $('#img01').width();
+    var height = $(img).height();
+    var width = $(img).width();
 
-    array.forEach(function (item) {
+    var faces = JSON.parse(array);
+
+    faces.forEach(function (item) {
         var marginLeft = (width * item[0]) / naturalWidth + (window.innerWidth - width) / 2;
         var marginTop = (height * item[1]) / naturalHeight + 100;
         var divWidth = (width * item[2]) / naturalWidth;
